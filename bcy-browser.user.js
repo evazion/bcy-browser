e0462ca67842ad57e9c52fe8f9702a8c17fd40b1
evazion <noizave@gmail.com>
2016-11-25 00:56:19 -0600

    [feat] Re-enable right click for works that disable it.

--- bcy-browser.user.js
+++ bcy-browser.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BCY.net Browser
 // @description  Allows browsing BCY.net without an account.
-// @version      1
+// @version      2
 // @author       evazion
 // @namespace    http://github.com/evazion/bcy-browser
 // @homepageURL  http://github.com/evazion/bcy-browser
@@ -15,3 +15,7 @@ Auth.unLogin = () => false;
 
 // Disable opening links in new tabs.
 $('[target="_blank"]').attr("target", "");
+
+// Artists have the option to disable downloads of their works, which just
+// disables right-clicking on their images. This re-enables it.
+$(".js-maincontent img, .lightbox-wrap, .lb-container").off("contextmenu dragstart click");
